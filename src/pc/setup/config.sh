b3fce7bb89fc4696af370b31bb2b852516635b01
ddaaggeett <david.g.daggett@gmail.com>
2022-08-27 12:06:01 -0400

    config.sh: wine + steam

--- src/pc/setup/config.sh
+++ src/pc/setup/config.sh
@@ -31,9 +31,13 @@ sudo apt install brave-browser -y
 sudo apt install git -y
 
 # install nvm
+# TODO: not working
+# TODO: untested
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install --lts
 
 # install python
+# TODO: untested
 wget -O python3.tar.gz https://www.python.org/ftp/python/3.10.6/Python-3.10.6.tar.xz
 tar xf python3.tar.xz && cd python3
 ./configure
@@ -56,6 +60,8 @@ sudo apt update
 sudo apt install obs-studio ffmpeg -y
 
 # install yarn
+# TODO: not working
+# TODO: untested
 npm install --global yarn
 
 # install rethinkDB
@@ -66,7 +72,23 @@ sudo apt-get update
 sudo apt-get install rethinkdb
 
 # install Expo-CLI
+# TODO: untested
 npm install --global expo-cli
+
+# install wine
+sudo sh -c 'echo " deb https://dl.winehq.org/wine-builds/debian/ bullseye main" >> /etc/apt/sources.list.d/wine.list'
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo apt update
+sudo apt install --install-recommends winehq-stable
+sudo apt install winetricks
+
+# install steam
+sudo dpkg --add-architecture i386
+sudo add-apt-repository non-free
+sudo apt install software-properties-common
+sudo apt update
+sudo apt install steam
 
 # set favorite app order 1. terminal 2. files 3. brave browser 4. atom.io 5. OBS studio
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop', 'brave-browser.desktop', 'atom.desktop', 'com.obsproject.Studio.desktop']"
