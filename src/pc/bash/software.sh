5b780b42db649a33f917b413ca2f71f7bd3e5461
ddaaggeett <david.g.daggett@gmail.com>
2022-10-08 21:34:08 -0400

    install yt-dlp

--- src/pc/bash/software.sh
+++ src/pc/bash/software.sh
@@ -73,3 +73,6 @@ sudo apt install steam
 # install youtube-dl
 sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
+# install yt-dlp
+sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/yt-dlp
+sudo chmod a+rx /usr/local/bin/yt-dlp  # Make executable
